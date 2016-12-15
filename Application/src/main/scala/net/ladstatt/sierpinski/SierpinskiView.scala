10e61f524cf3b578d81bd7b6fb7833daf14da3cb
SuperCari <carina.coelestin@edu.fh-joanneum.at>
2016-12-15 22:00:32 +0100

    Huihui

--- Application/src/main/scala/net/ladstatt/sierpinski/SierpinskiView.scala
+++ Application/src/main/scala/net/ladstatt/sierpinski/SierpinskiView.scala
@@ -41,7 +41,7 @@ class SierpinskiView(val context: Context, val attrs: AttributeSet) extends View
   }
 
   override def onDraw(canvas:Canvas) {
-    drawSierpinski(Sierpinski(Triangle(Pos(0, 0), 1080), 7), canvas, paint)
+    drawSierpinski(Sierpinski(Triangle(Pos(0, 0), 1080), 5), canvas, paint)
 
 
 
